3fffd164794f298af5de86db48d91ffadeb8637a
hguama <guamaoh@gmail.com>
2021-06-04 15:56:10 -0500

    msg

--- Semana3/4junio/clase4junio.py
+++ Semana3/4junio/clase4junio.py
@@ -1,10 +1,10 @@
 print("hola mundo")
 print("1")
 
-def factorial(n):
-    fact = 1
-    for i in range(1, n + 1):
-        fact = fact * i
-    return fact
-def sumar(a,b):
-    print(a,b)
+# def factorial(n):
+#     fact = 1
+#     for i in range(1, n + 1):
+#         fact = fact * i
+#     return fact
+# def sumar(a,b):
+#     print(a,b)
